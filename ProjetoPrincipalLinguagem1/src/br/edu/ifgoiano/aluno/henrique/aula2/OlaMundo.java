ad19acd9d3d6302638a43977d523a2bc7f3e27a5
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-02-04 13:23:29 -0300

    Projeto Como estava ontem

---
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula2/OlaMundo.java
@@ -0,0 +1,11 @@
+package br.edu.ifgoiano.aluno.henrique.aula2;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ola Mundo!");
+
+	}
+
+}
