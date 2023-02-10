42f4234027e3ac3b76b0eec7525d6fd5f9fe7842
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-02-10 10:44:38 -0300

    Fazendo algumas alteracoes

--- ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula2/OlaMundo.java
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula2/OlaMundo.java
@@ -1,10 +1,21 @@
 package br.edu.ifgoiano.aluno.henrique.aula2;
 
+import javax.swing.JOptionPane;
+
 public class OlaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ola Mundo!");
+		int a,b,multi;
+		a = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe um número"));
+		b = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe um número"));
+		
+		multi = a * b;
+		System.out.println(multi);
+		
+		
+	
+		
 
 	}
 
