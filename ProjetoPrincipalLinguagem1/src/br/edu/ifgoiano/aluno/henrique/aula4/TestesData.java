8f2ea4c7f97075b495f2a9b5e489cdfc25c57616
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-02-25 11:37:42 -0300

    Atualizado

--- ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula4/TestesData.java
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula4/TestesData.java
@@ -7,18 +7,18 @@ public class TestesData {
 		System.out.println("Data1: " + dataNova1.getDia() + "/" + dataNova1.getMes() + "/" + dataNova1.getAno());
 		
 		//Data Invalida, no dia.
-		 Data data2 = new Data(10, 4, 2022);
-	     System.out.println("Data2: " + data2.getDia() + "/" + data2.getMes() + "/" + data2.getAno());
+		Data data2 = new Data(10, 4, 2022);
+	    System.out.println("Data2: " + data2.getDia() + "/" + data2.getMes() + "/" + data2.getAno());
 
-	     System.out.println("-------------");
+	    System.out.println("-------------");
 	     
 	  // Teste com mês inválido
-	     Data data3 = new Data(15, 1, 2021);
-	     System.out.println("Data3: " + data3.getDia() + "/" + data3.getMes() + "/" + data3.getAno());
+	    Data data3 = new Data(15, 1, 2021);
+	    System.out.println("Data3: " + data3.getDia() + "/" + data3.getMes() + "/" + data3.getAno());
 
 	  // Teste com ano inválido
-	     Data data4 = new Data(10, 8, 2020);
-	     System.out.println("Data4: " + data4.getDia() + "/" + data4.getMes() + "/" + data4.getAno());
+	    Data data4 = new Data(10, 8, 2020);
+	    System.out.println("Data4: " + data4.getDia() + "/" + data4.getMes() + "/" + data4.getAno());
 	     
 	     
 		
