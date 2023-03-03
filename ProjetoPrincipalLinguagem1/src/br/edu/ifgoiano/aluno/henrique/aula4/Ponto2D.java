f2d676d43ae6e3f89a13ff2dbfdf19f02f4cc432
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-03-03 11:14:08 -0300

    Ultimo terminado

---
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula4/Ponto2D.java
@@ -0,0 +1,34 @@
+package br.edu.ifgoiano.aluno.henrique.aula4;
+
+public class Ponto2D {
+	private double x;
+	private double y;
+	
+	public Ponto2D() {
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public Ponto2D(double x1, double y1) {
+		this.x = x1;
+		this.y = y1;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	
+	
+	
+
+}
---
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula4/TestesPonto2D.java
@@ -0,0 +1,16 @@
+package br.edu.ifgoiano.aluno.henrique.aula4;
+
+public class TestesPonto2D {
+
+	public static void main(String[] args) {
+		Ponto2D pont = new Ponto2D(7,8);
+	    System.out.println("Os pontos são " + "(" + pont.getX() +  "," + pont.getY() + ")");
+
+	    System.out.println("-----------------------");
+
+	    Ponto2D pont2 = new Ponto2D();
+	    System.out.println("Os pontos são " + "(" + pont2.getX() +  "," + pont2.getY() + ")");
+
+	}
+
+}
