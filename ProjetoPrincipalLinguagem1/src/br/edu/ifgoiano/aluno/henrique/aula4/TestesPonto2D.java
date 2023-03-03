d91473ddd1b013add3122d6abd9db1895af9eed2
Henrique Gabriel <75758371+HenriqueGabriel2022@users.noreply.github.com>
2023-03-03 11:16:20 -0300

    Arrumado

--- ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula4/TestesPonto2D.java
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula4/TestesPonto2D.java
@@ -3,7 +3,7 @@ package br.edu.ifgoiano.aluno.henrique.aula4;
 public class TestesPonto2D {
 
 	public static void main(String[] args) {
-		Ponto2D pont = new Ponto2D(7,8);
+	    Ponto2D pont = new Ponto2D(7,8);
 	    System.out.println("Os pontos são " + "(" + pont.getX() +  "," + pont.getY() + ")");
 
 	    System.out.println("-----------------------");
