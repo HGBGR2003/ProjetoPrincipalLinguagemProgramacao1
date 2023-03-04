85d54cdb8dc44006508c6698197f0681c5e91793
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-03-04 09:32:05 -0300

    Terminado

--- ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula6/TestesGerador.java
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula6/TestesGerador.java
@@ -3,7 +3,12 @@ package br.edu.ifgoiano.aluno.henrique.aula6;
 public class TestesGerador {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		GeradorDeArray gerador = new GeradorDeArray(5);
+		String palavra = gerador.indeceCorrespondente(2);
+		System.out.println(palavra);
+		
+		palavra = gerador.indeceCorrespondente(1);
+		System.out.println(palavra);
 
 	}
 
