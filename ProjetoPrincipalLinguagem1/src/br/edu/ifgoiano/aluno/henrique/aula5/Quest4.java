75a61cb7d2e9acbecc8d044eeebb90fbc133d36b
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-03-04 08:54:25 -0300

    Terminado

--- ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula5/Quest4.java
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula5/Quest4.java
@@ -1,19 +1,20 @@
 package br.edu.ifgoiano.aluno.henrique.aula5;
 
-public class Quest4 {
+import java.util.Arrays;
 
+public class Quest4 {
 	public static void main(String[] args) {
-		int[][] vetor4 = new int[6][6];
+		double[][] vetor4 = new double[6][6];
 		
-		for(int l = 0; l < 6; l++) {
-			for(int c = 0; c < 6; c++) {
+		for(int l = 0; l < vetor4.length; l++) {
+			for(int c = 0; c < vetor4[l].length; c++) {
 				vetor4[l][c] = -1 * ((l + 1) % 2);
 			}
 		}
 		
-		//for(int i = 0; i < ) {
-			
-		//}
+		for(int i = 0; i < vetor4.length; i++) {
+			System.out.println(Arrays.toString(vetor4[i]));
+		}
 
 	}
 
