66685f31797800005f027a2c74a52a8986ff465d
Henrique Gabriel Barbosa Guida Rodrigues <2022108203030033@ifgoiano.local>
2023-03-10 20:20:32 -0300

    Comecando

---
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula7/Funcionario.java
@@ -0,0 +1,30 @@
+package br.edu.ifgoiano.aluno.henrique.aula7;
+
+public class Funcionario {
+	private String nome;
+	private String matricula;
+	private double salarioBase;
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public double getSalarioBase() {
+		return salarioBase;
+	}
+	public void setSalarioBase(double salarioBase) {
+		this.salarioBase = salarioBase;
+	}
+
+	
+}
