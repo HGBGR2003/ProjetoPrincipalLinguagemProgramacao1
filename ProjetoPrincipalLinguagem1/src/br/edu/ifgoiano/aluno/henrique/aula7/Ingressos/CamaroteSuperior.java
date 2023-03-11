eae4e78f4b4acdf3ed2bf6bcfae5583eeeb658aa
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-03-11 16:51:42 -0300

    Terminado e com duvidas

--- ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula7/Ingressos/CamaroteSuperior.java
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula7/Ingressos/CamaroteSuperior.java
@@ -1,5 +1,19 @@
 package br.edu.ifgoiano.aluno.henrique.aula7.Ingressos;
 
 public class CamaroteSuperior extends Vip {
+	double valorAdicional;
 
+	public double getValorAdicional() {
+		return valorAdicional;
+	}
+
+	public void setValorAdicional(double valorAdicional) {
+		this.valorAdicional = valorAdicional;
+	}
+	
+	public double ValorDoSuperior(double adicional) {
+		this.valorAdicional = adicional;
+		return ValorDoSuperior(adicional);
+	}
+	
 }
