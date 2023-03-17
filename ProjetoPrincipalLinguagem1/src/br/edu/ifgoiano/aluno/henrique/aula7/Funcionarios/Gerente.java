b77af4e4efc3ea0a02159991da7f378274fd8a43
Henrique Gabriel Barbosa Guida Rodrigues <2022108203030033@ifgoiano.local>
2023-03-17 20:37:47 -0300

    Arrumando

--- ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula7/Funcionarios/Gerente.java
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula7/Funcionarios/Gerente.java
@@ -1,9 +1,11 @@
 package br.edu.ifgoiano.aluno.henrique.aula7.Funcionarios;
 
 public class Gerente extends Funcionario {
+	String localDeLotacao;
 
-	public Gerente(String nome, String matricula, double salarioBase) {
+	public Gerente(String nome, String matricula, double salarioBase, String localDelotacao) {
 		super(nome, matricula, salarioBase);
+		this.localDeLotacao = localDelotacao;
 	}
 
 }
--- ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula7/Funcionarios/TestesFuncionario.java
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula7/Funcionarios/TestesFuncionario.java
@@ -3,12 +3,13 @@ package br.edu.ifgoiano.aluno.henrique.aula7.Funcionarios;
 public class TestesFuncionario {
 
 	public static void main(String[] args) {
+		Funcionario funcio = new Funcionario("A", "B", 1000);
 		Vendedor vend1 = new Vendedor("Mario", "1453", 3512.67);
 		Assistente assis1 = new Assistente("Marcola", "145623021", 1444);
-		Gerente gere1 = new Gerente("Murilo", "789213", 7800);
+		Gerente gere1 = new Gerente("Nome","4545", 1450, "L");
 		
 		double soma;
-		soma = gere1.getSalarioBase() + assis1.getSalarioBase() + vend1.getSalarioBase();
+		soma = funcio.getSalarioBase() + gere1.getSalarioBase() + assis1.getSalarioBase() + vend1.getSalarioBase();
 		System.out.println("A soma dos valores é " + soma);
 		
 	}
