6297aefa9a8a4da503be4616c144d22f6e76ab02
Henrique Gabriel Barbosa Guida Rodrigues <2022108203030033@ifgoiano.local>
2023-03-17 22:06:28 -0300

    Feito

--- ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula8/Animais/Peixe.java
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula8/Animais/Peixe.java
@@ -4,14 +4,22 @@ public class Peixe extends Animal {
 
 	String caracteristica;
 	
+	public String getCaracteristica() {
+		return caracteristica;
+	}
+
+	public void setCaracteristica(String caracteristica) {
+		this.caracteristica = caracteristica;
+	}
+
 	public Peixe(String nome, float comprimento, int numeroDEPastas, String cor, String ambiente, float velocidade, String caracteristicas) {
 		super(nome, comprimento, numeroDEPastas, cor, ambiente, velocidade);
 		this.caracteristica = caracteristicas;
 	}
 	
 	public void dadosDOPeixe() {
-		System.out.println(this.nome + "," + this.comprimento + "," + this.numeroDEPastas + "," + this.cor
-				+ "," + this.ambiente + "," + this.velocidade + "," + this.caracteristica);
+		System.out.println("O nome do animal é " + this.nome + " ela tem " + this.comprimento + " metros e seu número de (patas) é " + this.numeroDEPastas + ", e sua cor é " + this.cor
+				+ "\nlogo o seu ambiente se encontra no " + this.ambiente + " há " + this.velocidade + " Km/h e finalizando esse animal é " + this.caracteristica + ".");
 	}
 
 }
--- ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula8/Animais/Teste.java
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula8/Animais/Teste.java
@@ -1,5 +1,9 @@
 package br.edu.ifgoiano.aluno.henrique.aula8.Animais;
 
 public class Teste {
+	public static void main(String[] args) {
+		Peixe novoAnimalMarinho = new Peixe("Baleia", 25,4,"Azul","Oceano",47,"pesado");
+		novoAnimalMarinho.dadosDOPeixe();
+	}
 
 }
