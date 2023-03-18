e38c61d02960bffbd60d1be2f16cd49baaf687d2
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-03-18 11:15:06 -0300

    Descartando espacos

--- ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula7/Ingressos/TestesIngresso.java
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula7/Ingressos/TestesIngresso.java
@@ -1,7 +1,7 @@
 package br.edu.ifgoiano.aluno.henrique.aula7.Ingressos;
 
 public class TestesIngresso {
-
+	
 	public static void main(String[] args) {
 		Ingresso i1 = new Ingresso();
 		Vip vi1 = new Vip();
