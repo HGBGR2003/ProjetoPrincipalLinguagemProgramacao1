016b16bc2871da1ae170a1fecf0922bf1c985a24
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2023-03-18 11:12:37 -0300

    3

--- ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula7/Ingressos/Normal.java
+++ ProjetoPrincipalLinguagem1/src/br/edu/ifgoiano/aluno/henrique/aula7/Ingressos/Normal.java
@@ -3,7 +3,7 @@ package br.edu.ifgoiano.aluno.henrique.aula7.Ingressos;
 public class Normal extends Ingresso {
 
 	public void imprimirNormalIngresso() {
-		System.out.println("Ingresso Normal");
+		System.out.println("Ingresso Normal: R$ " + this.valor);
 	}
 	
 	
